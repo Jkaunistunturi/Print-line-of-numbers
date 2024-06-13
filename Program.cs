07f26c7baf3b9e0bc12d4c74d22bc2abbe280251
JK <jenny.kaunistunturi@edunord.se>
2024-06-13 15:05:14 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace nac
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("How many numbers you want to print?");
+            int enterNumber = Convert.ToInt32(Console.ReadLine());
+            
+            for (int i = 0; i <= enterNumber; i++)
+            {
+                Console.Write(i);
+            }
+
+        }
+
+    }
+}
